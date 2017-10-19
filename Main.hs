c42855195c56d529fb402ae6707f452534fc079d
Manuel Bärenz <maths@manuelbaerenz.de>
2017-10-19 15:45:25 +0200

    Some ideas

--- Main.hs
+++ Main.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE DataKinds #-}
-
--- base
-import System.IO (hFlush, stdout)
+{-# LANGUAGE Arrows       #-}
+{-# LANGUAGE DataKinds    #-}
+{-# LANGUAGE RankNTypes   #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- dunai
-import Control.Monad.Trans.MSF.Maybe (listToMaybeS, runMaybeT, MaybeT)
+import Control.Monad.Trans.MSF.Except
 
 -- rhine
 import FRP.Rhine
@@ -13,11 +13,31 @@ import FRP.Rhine.Clock.Realtime.Millisecond
 
 -- TODO Need to generalise the IO clocks to MonadIO
 main :: IO ()
-main = do
-  _ <- runMaybeT $ flow mainRhine
-  return ()
+main = flow mainRhine
 
-mainRhine :: Rhine (MaybeT IO) (LiftClock IO MaybeT (Millisecond 50)) () ()
-mainRhine =   timeless (listToMaybeS "Congratulations! You've installed the tutorial correctly!\n")
-          >-> liftS (putChar >>> (>> hFlush stdout))
-          @@  liftClock waitClock
+
+data Tea = Tea
+  { teaSort  :: String -- ^ The sort, brand, type of tea
+  , duration :: Double -- ^ The duration to brew in minutes
+  }
+
+-- TODO Also record intermediate step where we just output a string instead of throwing an exception
+countdownTea :: Tea -> Behaviour (ExceptT String m) Double ()
+countdownTea Tea {} = proc _ -> do
+  timeSoFar <- integral        -< 1 / 60 -- TODO Can we do something better like measuring the time on start
+  _         <- timeless (throwOn teaSort) -< timeSoFar >= duration
+  returnA                      -< ()
+
+testTea = Tea
+  { teaSort  = "Earl Grey"
+  , duration = 3
+  }
+
+-- TODO To make this work in a readable way, dunai should really switch to Arrow Transformer classes
+teaLoop = safely $ do
+  teaSort <- try $ countdownTea testTea
+  _ <- once_ $ putStrLn $ "Your " ++ teaSort ++ " is ready!"
+  safe teaLoop -- TODO Alternatively do the safely outside
+
+mainRhine :: Rhine IO (Millisecond 50) () ()
+mainRhine = teaLoop @@ waitClock
