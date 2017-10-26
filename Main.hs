7a6f368e90adf2c22229da03db6b76402af4c5ea
Manuel Bärenz <manuel@enigmage.de>
2017-10-26 22:13:20 +0200

    Fixed Main.hs to dunai 0.1.1.0

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Control.Concurrent (threadDelay)
 import System.IO (hFlush, stdout)
 
 -- dunai
-import Control.Monad.Trans.MSF.Maybe (listToMaybeS, runMaybeT, MaybeT)
+import Control.Monad.Trans.MSF.Maybe (runMaybeT, MaybeT, exit)
 
 -- rhine
 import FRP.Rhine
@@ -22,3 +22,8 @@ mainRhine :: Rhine (MaybeT IO) (LiftClock IO MaybeT (Millisecond 50)) () ()
 mainRhine =   timeless (listToMaybeS "Congratulations! You've installed the tutorial correctly!\n")
           >-> liftS (putChar >>> (>> hFlush stdout))
           @@  liftClock waitClock
+
+
+-- TODO In dunai 0.1.2
+listToMaybeS :: Monad m => [b] -> MSF (MaybeT m) a b
+listToMaybeS = foldr iPost exit
